934f6841e9e0827572badd6f5e44c329be94cc8e
DESKTOP-8TCPMCB\Liem Phan <minhliemphp@gmail.com>
2017-06-26 19:45:05 +0700

    update hmr

--- app/Modules/Front/routes.php
+++ app/Modules/Front/routes.php
@@ -1,11 +1,14 @@
 <?php
 
 Route::group(['middleware' => ['web','language'],'namespace'=>'App\Modules\Front\Controllers'],function(){
+  Route::get('/test',function(){
+    return trans('routes.contact');
+  });
   Route::get('/',['as'=>'front.getIndex','uses'=>'HomeController@index']);
   Route::get('/recent-project',['as'=>'front.getRecent','uses'=>'HomeController@index']);
   Route::get('/agency/{slug?}',['as'=>'front.getAgency','uses'=>'ClientController@index']);
   Route::get('/projects',['as'=>'front.getProject','uses'=>'ProjectController@index']);
-  Route::get('/contact/{param?}',['as'=>'front.getContact','uses'=>'ContactController@index']);
+  Route::get(trans("routes.contact").'/{param?}',['as'=>'front.getContact','uses'=>'ContactController@index']);
   Route::post('/contact',['as'=>'front.postContact','uses'=>'ContactController@postIndex']);
 
 });
---
+++ resources/lang/vi/routes.php
@@ -0,0 +1,5 @@
+<?php
+return [
+  'about' => 've-chung-toi',
+  'contact'=>'lien-he'
+];
